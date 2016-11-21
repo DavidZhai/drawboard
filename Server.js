8a30953ad02a9802f3dceba691ca50a6099fb29d
Hao Zhai <davidzh@D-10-155-102-127.dhcp4.washington.edu>
2016-11-20 16:08:01 -0800

    lol nothing works

--- Server.js
+++ Server.js
@@ -1,20 +1,19 @@
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
-const PORT = 41234;
-const HOST = 'localhost';
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-server.on('error', (err) => {
-  console.log(`server error:\n${err.stack}`);
-  server.close();
+var PORT = 8080;
+
+// serve the client on the top level
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/drawboard.html');
 });
 
-server.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
-server.on('listening', () => {
-  var address = server.address();
-  console.log(`server listening ${address.address}:${address.port}`);
-});
 
-server.bind(PORT, HOST);
+http.listen(PORT, function() {
+  console.log('listening on 8080');
+});
