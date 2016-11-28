e54c12603bdcac5bfc18e928587e6fdea776d321
root <DavidZhai@github.com>
2016-11-27 23:32:10 -0800

    basic drawingpanel working- no server

--- static/drawboard.js
+++ static/drawboard.js
@@ -1,23 +1,52 @@
 var socket = io();
 
 var panel = document.getElementById('drawingPanel');
-
-/* needed to find relative location to window */
-var rect = panel.getBoundingClientRect();
-var canvasTop = parseInt(rect.top);
-var canvasLeft = parseInt(rect.left);
+var isDrawing = false;  //toggle for drawing 
 
 panel.addEventListener('mousemove', function(e) {
-  myFunction(e);
+  draw(e);
  // socket.emit()
 });
 
-function myFunction(e) {
+panel.addEventListener("mousedown", function(e) {
+  isDrawing = true;
+});
+
+panel.addEventListener("mouseup", function(e) {
+  isDrawing = false;
+  console.log("mouseup");
+})
+
+function draw(e) {
+
+  /* needed to find relative location to window */
+  var rect = panel.getBoundingClientRect();
+  var canvasTop = parseInt(rect.top);
+  var canvasLeft = parseInt(rect.left);
+
+  // need to convert to canvas coordinates, look into factoring this out
+  
   var x = e.clientX - canvasLeft;
   var y = e.clientY - canvasTop;
+  if (isDrawing) {
+    var pen = panel.getContext("2d");
+    pen.moveTo(previousX, previousY);
+    pen.lineTo(x, y);
+    pen.stroke();
+  }
+  previousX = x;
+  previousY = y;
+
   var coor = "Coordinates: (" + x + "," + y + ")";
   document.getElementById("demo").innerHTML = coor;
 }
+
+// function drawOnMouse(e) {
+//   var pen = panel.getContext("2d");
+//   pen.moveTo(0, 0);
+//   pen.lineTo(e.clientX - canvasLeft ,e.clientY - canvasTop);
+//   pen.stroke();  
+// }
 
    /*
       $('form').submit(function(){
